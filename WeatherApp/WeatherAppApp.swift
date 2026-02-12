ce3d24d2688c83a2402dc434c7984d78be396915
Valentyna Kharkova <gerry29072001@gmail.com>
2026-02-12 12:27:07 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Valentyna Kharkova on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
