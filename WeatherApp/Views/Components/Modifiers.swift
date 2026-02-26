adade51701b0d78b3d199dfe2daac4df134e683f
Valentyna Kharkova <gerry29072001@gmail.com>
2026-02-26 11:21:59 +0200

    [Feature] Added App Icon

---
+++ WeatherApp/Views/Components/Modifiers.swift
@@ -0,0 +1,18 @@
+//
+//  Modifiers.swift
+//  WeatherApp
+//
+//  Created by Valentyna Kharkova on 24.02.2026.
+//
+
+import SwiftUI
+
+struct Modifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Modifiers()
+}
